f74f1550879a511e16104ee3df647299db53f0e0
“Mhaventhan” <mhaventhan@gmail.com>
2018-10-15 16:00:04 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# Elasticsearch
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: Elasticsearch
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
