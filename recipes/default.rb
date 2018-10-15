9dfeb1d4675e797cc5e9c59f2994f57912522cfb
“Mhaventhan” <mhaventhan@gmail.com>
2018-10-15 16:20:54 +0100

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+apt_update "update_sources" do
+  action :update
+end
+
+package "openjdk-8-jdk" do
+  action :install
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,25 +11,16 @@ describe 'Elasticsearch::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should install java' do
+      expect(chef_run).to install_package("openjdk-8-jdk")
     end
   end
 end
